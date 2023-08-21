232d45ee5c58cb0077f80473e7cb143e846601f2
RayhaanK <rayhaankhan331@gmail.com>
2023-08-21 11:15:08 +0200

    controller Users

--- API/controller/index.js
+++ API/controller/index.js
@@ -1,1 +1,30 @@
 // Controller
+const express = require('express')
+const bodyParser = require('body-parser')
+const {verrifyAToken} = require('../middleware/AuthenticateUser')
+const routes = express.Router()
+const {users} = require('../model')
+
+// ================User's Router=================
+routes.get('/users', (req, res)=>{
+    users.fetchUsers(req, res)
+})
+routes.get('/user/:id', (req, res)=>{
+    users.fetchUser(req, res)
+})
+routes.post('/user', bodyParser.json(), (req, res)=>{
+    users.register(req, res)
+})
+routes.post('/user',
+bodyParser.json(), (req, res)=>{
+    users.login(req, res)
+})
+routes.put('/user/:id', bodyParser.json(), (req, res)=>{
+    users.updateUser(req, res)
+})
+routes.patch('/user/:id', bodyParser.json(), (req, res)=>{
+    users.updateUser(req, res)
+})
+routes.delete('/user/:id', (req, res)=>{
+    users.deleteUser(req, res)
+})
