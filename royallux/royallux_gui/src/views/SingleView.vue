bd7a612e06e73dab0d44e0ba99eae14d297ea511
RayhaanK <rayhaankhan331@gmail.com>
2023-08-22 12:49:00 +0200

    products are displalyed

--- royallux/royallux_gui/src/views/SingleView.vue
+++ royallux/royallux_gui/src/views/SingleView.vue
@@ -1,6 +1,12 @@
 <template>
-    <div class="single">
-      <h1>This is a single page</h1>
-    </div>
-  </template>
-  
+ <div class="single">
+    <h1>This is a single page</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
